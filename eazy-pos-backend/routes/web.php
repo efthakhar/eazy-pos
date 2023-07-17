5a510f4f81a35571c8954fb353df90e78f35ad82
Efthakhar <efthakharbin@gmail.com>
2023-07-17 20:04:26 +0600

    Add user registration api route

--- eazy-pos-backend/routes/web.php
+++ eazy-pos-backend/routes/web.php
@@ -1,18 +1,14 @@
 <?php
 
+use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
+
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+// API Routes
+Route::post('/user-registration',[UserController::class,'UserRegistration']);
